3e6ce6a15a732ed39b750c64c0289c113bf9970c
Sean You <seon2you@hotmail.com>
2017-11-08 23:43:18 -0800

    add db table, db migration, update database

--- Ng4NetCoreAuth/Auth/JwtFactory.cs
+++ Ng4NetCoreAuth/Auth/JwtFactory.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 
 namespace Ng4NetCoreAuth.Auth
@@ -21,7 +22,10 @@ namespace Ng4NetCoreAuth.Auth
 
     public ClaimsIdentity GenerateClaimsIdentity(string userName, string id)
     {
-      throw new NotImplementedException();
+      return new ClaimsIdentity(new GenericIdentity(userName, "Token"), new[] {
+        new Claim(Helpers.Constants.Strings.JwtClaimIdentifiers.Id, id),
+        new Claim(Helpers.Constants.Strings.JwtClaimIdentifiers.Rol, Helpers.Constants.Strings.JwtClaims.ApiAccess)
+      });
     }
 
     public async Task<string> GenerateEncodedToken(string userName, ClaimsIdentity identity)
@@ -32,18 +36,51 @@ namespace Ng4NetCoreAuth.Auth
         new Claim(JwtRegisteredClaimNames.Jti, await _jwtOptions.JtiGenerator()),
         new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(_jwtOptions.IssuedAt).ToString(), ClaimValueTypes.Integer64),
         identity.FindFirst(Helpers.Constants.Strings.JwtClaimIdentifiers.Rol),
+        identity.FindFirst(Helpers.Constants.Strings.JwtClaimIdentifiers.Id)
+      };
 
+      // Create the JWT security token and encode it.
+      var jwt = new JwtSecurityToken(
+        issuer: _jwtOptions.Issuer,
+        audience: _jwtOptions.Audience,
+        claims: claims,
+        notBefore: _jwtOptions.NotBefore,
+        expires: _jwtOptions.Expiration,
+        signingCredentials: _jwtOptions.SigningCredentials
+      );
+
+      var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+      return encodedJwt;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="issuedAt"></param>
+    /// <returns>Date converted to seconds since Unix epoch (Jan 1, 1970, midnight UTC).</returns>
+    private static long ToUnixEpochDate(DateTime date)
+    {
+      return (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
+    }
+
+    private static void ThrowIfInvalidOptions(JwtIssuerOptions jwtOptions)
+    {
+      if (jwtOptions == null) throw new ArgumentNullException(nameof(jwtOptions));
+
+      if (jwtOptions.ValidFor <= TimeSpan.Zero)
+      {
+        throw new ArgumentException("Must be a non-zero TimeSpan.", nameof(JwtIssuerOptions.ValidFor));
       }
-    }
 
-    private object ToUnixEpochDate(DateTime issuedAt)
-    {
-      throw new NotImplementedException();
-    }
+      if (jwtOptions.SigningCredentials == null)
+      {
+        throw new ArgumentNullException(nameof(JwtIssuerOptions.SigningCredentials));
+      }
 
-    private void ThrowIfInvalidOptions(JwtIssuerOptions jwtOptions)
-    {
-      throw new NotImplementedException();
+      if (jwtOptions.JtiGenerator == null)
+      {
+        throw new ArgumentNullException(nameof(JwtIssuerOptions.JtiGenerator));
+      }
     }
   }
 }
--- Ng4NetCoreAuth/Startup.cs
+++ Ng4NetCoreAuth/Startup.cs
@@ -6,19 +6,45 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Ng4NetCoreAuth.Data;
+using Microsoft.EntityFrameworkCore;
+using Ng4NetCoreAuth.Auth;
 
 namespace Ng4NetCoreAuth
 {
     public class Startup
     {
+        private const string SecretKey = "iNivDmHLpUA223sqsfhqGbMRdRj1PVkH"; // todo: get this from somewhere secure
+        private readonly SymmetricSecurityKey _signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SecretKey));
+
+        public Startup(IHostingEnvironment env)
+        {
+          var builder = new ConfigurationBuilder()
+              .SetBasePath(env.ContentRootPath)
+              .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+              .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+              .AddEnvironmentVariables();
+          Configuration = builder.Build();
+        }
+        public IConfigurationRoot Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+          // Add framework services.
+          services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("Ng4NetCoreAuth")));
+
+          services.AddSingleton<IJwtFactory, JwtFactory>();
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
