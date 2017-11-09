c6edbb59a2595223b5d41d9b82342b54d392b0bd
Sean You <seon2you@hotmail.com>
2017-11-08 22:40:10 -0800

    automapper

---
+++ Ng4NetCoreAuth/ViewModels/Mappings/ViewModelToEntityMappingProfile.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+using Ng4NetCoreAuth.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ng4NetCoreAuth.ViewModels.Mappings
+{
+    public class ViewModelToEntityMappingProfile : Profile
+    {
+      public ViewModelToEntityMappingProfile()
+      {
+        CreateMap<RegistrationViewModel, AppUser>()
+          .ForMember(au => au.UserName, map => map.MapFrom(vm => vm.Email));
+      }
+    }
+}
